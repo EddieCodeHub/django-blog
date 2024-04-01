964acde79d296a19d24fb35b20bea12b23f45047
EddieCodeHub Meekin <eddiemeekincode@gmail.com>
2024-04-01 11:04:08 +0000

    docs: prepare code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k4x-1lq)+=6qgw01j5a83md*-e!3k(+q+b2*=um04gz-d0-__d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
