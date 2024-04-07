55de109a5ebda817b082fe8e7772982536e43a35
EddieCodeHub Meekin <eddiemeekincode@gmail.com>
2024-04-07 12:55:15 +0000

    docs: alter comment and post options

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on", "author"]
     
@@ -27,6 +28,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+
     class Meta:
         ordering = ["-created_on"]
     
