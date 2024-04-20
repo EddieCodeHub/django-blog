88c05f1a1ce885cba22a07a6927c9160083df69c
EddieCodeHub Meekin <eddiemeekincode@gmail.com>
2024-04-20 20:06:10 +0000

    docs: handle collaboration form post request and save to database

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from django.contrib import messages
 from .forms import CollaborateForm
 
 # Create your views here.
@@ -9,6 +10,16 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
+    
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request submitted'
+            )
+            collaborate_form = CollaborateForm()
 
     return render(
         request,
